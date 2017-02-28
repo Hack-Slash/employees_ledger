12663bdd8f619ef316ae5429f5a0682d728c3695
Brian <riceb53@gmail.com>
2017-02-28 17:54:54 -0500

    Add attr_readers and attr_writer

--- employee.rb
+++ employee.rb
@@ -48,31 +48,14 @@
 # p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
-  end
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
 
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def active=(active_input)
-    @active = active_input
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def give_annual_raise
@@ -84,12 +67,11 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Manila", "Campos", 80000, true)
-employee2 = Employee.new("Danilo", "Carter", 70000, false)
+employee1 = Employee.new({first_name: "Manila", last_name: "Campos", salary: 80000, active: true})
 
+# employee2 = Employee.new("Danilo", "Carter", 70000, false)
 employee1.print_info
-# p employee2.give_annual_raise
-# p employee1.give_annual_raise
-p employee1.active
+
+# p employee1.active
 employee1.active = false
-p employee1.active
+# p employee1.active
