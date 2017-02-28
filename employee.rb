9668aedb802badb8df1add79241f0d14bf366a5d
Brian <riceb53@gmail.com>
2017-02-27 20:13:03 -0500

    Add class

--- employee.rb
+++ employee.rb
@@ -26,23 +26,42 @@
 
 # p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year"
 
-# hash with symbols
-#  'traditional syntax'
-employee1 = {
-  :first_name => "Manila",
-  :last_name => "Campos",
-  :salary => 80000,
-  :active => true
-}
+# # hash with symbols
+# #  'traditional syntax'
+# employee1 = {
+#   :first_name => "Manila",
+#   :last_name => "Campos",
+#   :salary => 80000,
+#   :active => true
+# }
 
-# fancy js syntax
-employee2 = {
-  first_name: "Danilo",
-  last_name: "Carter",
-  salary: 70000,
-  active: false
-}
+# # fancy js syntax
+# employee2 = {
+#   first_name: "Danilo",
+#   last_name: "Carter",
+#   salary: 70000,
+#   active: false
+# }
 
-p "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]} per year"
+# p "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]} per year"
 
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year"
+  end
+end
+
+employee1 = Employee.new("Manila", "Campos", 80000, true)
+employee2 = Employee.new("Danilo", "Carter", 70000, false)
+
+employee1.print_info
+employee2.print_info
