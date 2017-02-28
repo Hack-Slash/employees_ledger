800cd10fd154c258559f6e4fb2539a1df505e4ed
Brian <riceb53@gmail.com>
2017-02-27 19:31:42 -0500

    Hash with strings

--- employee.rb
+++ employee.rb
@@ -1,8 +1,28 @@
-employee1 = ["Manila", "Campos", 80000, true]
-employee2 = ["Danilo", "Carter", 70000, false]
+# array
+# employee1 = ["Manila", "Campos", 80000, true]
+# employee2 = ["Danilo", "Carter", 70000, false]
 
-# Manila Campos makes $80000 per year
-p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year"
+# # Manila Campos makes $80000 per year
+# p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year"
 
-p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
+
+# hash with strings
+employee1 = {
+  "first_name" => "Manila",
+  "last_name" => "Campos",
+  "salary" => 80000,
+  "active" => true
+}
+
+employee2 = {
+  "first_name" => "Danilo",
+  "last_name" => "Carter",
+  "salary" => 70000,
+  "active" => false
+}
+
+p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
+
+p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year"
 
