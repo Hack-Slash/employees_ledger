cd3c3fc051d15bb39bc85ae35cd571c7badec52d
Brian <riceb53@gmail.com>
2017-03-08 16:57:56 -0500

    Add oop

--- oop_exercise.rb
+++ oop_exercise.rb
@@ -5,44 +5,107 @@
 # 5. CHALLENGE: Change the program to give the user the choice at the end of the game to retry the cards they got wrong.
 # 6. CHALLENGE: Change the interface with better prompts, ASCII art, etc. Be as creative as you'd like!
 
+# class Card
+#   attr_reader :answer, :question
+#   def initialize(input_options)
+#     @answer = input_options[:answer]
+#     @question = input_options[:question]
+#   end
+# end
+
+# class Deck
+#   attr_reader :cards
+
+#   def initialize(input_options)
+#     make_cards(input_options)
+#   end
+
+#   def make_cards(hash_of_cards)
+#     @cards = []
+#     hash_of_cards.each do |question, answer|
+#       @cards << Card.new(question: question, answer: answer)
+#     end
+#   end
+
+#   def remaining_cards
+#     @cards.count
+#   end
+
+#   def draw_card
+#     @cards.delete_at(rand(@cards.length))
+#   end
+# end
+
+# 2. Change the program to use multiple choice questions. The Card class should be responsible for checking the answer.
+
 class Card
-  attr_reader :question, :answer
-  def initialize(input_question, input_answer)
-    @question = input_question
-    @answer = input_answer
+  def initialize(input_options)
+    @answer = input_options[:answer]
+    @question = input_options[:question]
+  end
+
+  def answer
+    # return whichever letter is associated with the value that is @answer
+    @mc_answers.each do |letter, answer|
+      if answer == @answer
+        return letter
+      end
+    end
+  end
+
+  def question
+    if @question == "What is the capital of Illinois?"
+      @mc_letters = ['a','b','c','d']
+      @answers = [@answer, "Helena", "Albany", "Austin"]
+    elsif @question == "Is Africa a country or a continent?"
+      @mc_letters = ['a','b']
+      @answers = [@answer, "Country"]
+    elsif @question == "Tug of war was once an Olympic event. True or false?"
+      @mc_letters = ['a','b']
+      @answers = [@answer, "False"]
+    end
+    # make into a hash
+      @mc_answers = {}
+      @mc_letters.each do |letter|
+        the_answer = @answers.delete_at(rand(@answers.length))
+        @mc_answers[letter] = the_answer
+        @question += "[#{letter}] #{the_answer}"
+      end
+      return @question
   end
 end
 
 class Deck
+  attr_reader :cards
+
   def initialize(input_options)
-    # create some new cards
-    # call make_cards from the initialize
     make_cards(input_options)
   end
 
-  def make_cards(some_cards)
+  def make_cards(hash_of_cards)
     @cards = []
-    some_cards.each do |hash_question, hash_answer|
-      # create a new card for each key value pair
-      @cards << Card.new(hash_question, hash_answer)
+    hash_of_cards.each do |question, answer|
+      @cards << Card.new(question: question, answer: answer)
     end
   end
 
   def remaining_cards
-    # return the number of remaining cards
-    @cards.length
+    @cards.count
   end
 
   def draw_card
-    # remove a card from the deck
-    @cards = @cards.shuffle
-    removed_card = @cards.pop
-    # return the card I removed
-    return removed_card
+    @cards.delete_at(rand(@cards.length))
   end
 end
 
-# not touch the code below here (until the bonus)
+
+# 3. CHALLENGE: Change the program to allow the user to retry once if they get the wrong answer.
+
+
+# 4. CHALLENGE: Change the program to keep track of number right/wrong and give a score at the end.
+# 5. CHALLENGE: Change the program to give the user the choice at the end of the game to retry the cards they got wrong.
+# 6. CHALLENGE: Change the interface with better prompts, ASCII art, etc. Be as creative as you'd like!
+
 trivia_data = {
   "What is the capital of Illinois?" => "Springfield",
   "Is Africa a country or a continent?" => "Continent",
@@ -53,11 +116,19 @@ deck = Deck.new(trivia_data) # deck is an instance of the Deck class
 
 while deck.remaining_cards > 0
   card = deck.draw_card # card is an instance of the Card class
-  puts card.question
-  user_answer = gets.chomp
-  if user_answer.downcase == card.answer.downcase
-    puts "Correct!"
-  else
-    puts "Incorrect!"
-  end
+  wrong_answers = 0
+  the_question = card.question
+    # this is for the first bonus
+    # while wrong_answers < 2
+      puts the_question
+      user_answer = gets.chomp
+      if user_answer.downcase == card.answer.downcase
+        puts "Correct!"
+        # break
+      else
+        puts "Incorrect!"
+        wrong_answers += 1
+      end
+      #  this is for the first bonus
+    # end
 end
